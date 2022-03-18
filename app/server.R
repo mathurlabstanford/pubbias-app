5e30d05aaf57343fd726e580cd0467dab5d82056
Mika Braginsky <mika.br@gmail.com>
2022-03-18 15:12:42 -0400

    first draft app

--- app/server.R
+++ app/server.R
@@ -1,26 +1,206 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(glue)
+library(tidyverse)
+library(PublicationBias)
 
-# Define server logic required to draw a histogram
+ci_text <- function(estimate, ci_lower, ci_upper, sig = 2) {
+  glue("{signif(estimate, sig)} (95% CI [{signif(ci_lower, sig)},
+        {signif(ci_upper, sig)}])")
+}
+sval_print <- function(sval) if (is.numeric(sval)) signif(sval, 2) else sval
+
+
 shinyServer(function(input, output) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
-    })
-
+  
+  # ----------------------------------------------------------------------------
+  # overall inputs
+  # ----------------------------------------------------------------------------
+  
+  meta_data <- reactive({
+    meta_file <- input$meta_data
+    ext <- tools::file_ext(meta_file$datapath)
+    
+    req(meta_file)
+    validate(need(ext == "csv", "Please upload a csv file"))
+    
+    read_csv(meta_file$datapath) |> mutate("unique_id" = 1:n(), .before = 1)
+    
+    # example_df <- metafor::escalc(measure = "RR", ai = tpos, bi = tneg,
+    #                               ci = cpos, di = cneg, data = metafor::dat.bcg)
+    # example_df$yi <- -example_df$yi
+    # example_df
+  })
+  
+  output$y_cols <- renderUI({
+    req(meta_data)
+    selectInput("y_col", "Column of point estimates",
+                choices = c("Select a column" = "", names(meta_data())))
+  })
+  
+  output$v_cols <- renderUI({
+    req(meta_data)
+    selectInput("v_col", "Column of estimated variances",
+                choices = c("Select a column" = "", names(meta_data())))
+  })
+  
+  output$direction <- renderUI({
+    req(input$v_col)
+    selectInput("direction", "Direction",
+                choices = c("favor affirmative", "favor non-affirmative"))
+  })
+  
+  output$model_type <- renderUI({
+    req(input$v_col)
+    selectInput("model_type", "Model type", choices = c("fixed", "robust"))
+  })
+  
+  output$cluster_cols <- renderUI({
+    if (req(input$model_type) == "robust") {
+      selectInput("cluster_col", "Column of cluster labels",
+                  choices = names(meta_data()))
+    }
+  })
+  
+  affirmative <- reactive({
+    req(input$direction)
+    str_detect(input$direction, "\\saffirmative")
+  })
+  
+  cluster_col <- reactive({
+    cc <- input$cluster_col
+    if (is.null(cc) || cc == "") "unique_id" else cc
+  })
+  
+  # ----------------------------------------------------------------------------
+  # corrected_meta
+  # ----------------------------------------------------------------------------
+  
+  output$eta_slider <- renderUI({
+    req(uncorrected_model())
+    sliderInput("eta", "η (ratio)", value = 2, min = 1, max = 20, step = 0.1)
+  })
+  
+  uncorrected_model <- reactive({
+    req(input$y_col, input$v_col)
+    metafor::rma(yi = meta_data()[[input$y_col]],
+                 vi = meta_data()[[input$v_col]],
+                 method = "FE") |>
+      broom::tidy(conf.int = TRUE)
+  })
+  
+  corrected_model <- reactive({
+    req(input$eta, input$y_col, input$v_col, cluster_col, affirmative,
+        input$model_type)
+    corrected_meta(yi = meta_data()[[input$y_col]],
+                   vi = meta_data()[[input$v_col]],
+                   eta = input$eta,
+                   clustervar = meta_data()[[cluster_col()]],
+                   model = input$model_type,
+                   favor.positive = affirmative())
+  })
+  
+  output$uncorrected <- renderUI({
+    req(uncorrected_model)
+    um <- uncorrected_model()
+    p(strong("Uncorrected estimate (log RR):"), br(),
+      ci_text(um$estimate, um$conf.low, um$conf.high))
+  })
+  
+  output$corrected <- renderUI({
+    req(corrected_model)
+    cm <- corrected_model()
+    p(strong("Corrected estimate  (log RR):"), br(),
+      ci_text(cm$est, cm$lo, cm$hi))
+  })
+  
+  output$corrected_summary <- renderUI({
+    req(input$eta, input$y_col, input$v_col, affirmative)
+    more_likely <- if (affirmative()) "affirmative" else "non-affirmative"
+    less_likely <- if (affirmative()) "non-affirmative" else "affirmative"
+    cm <- corrected_model()
+    p(em(glue("If {more_likely} studies were {input$eta} times more likely
+              to be published than {less_likely} studies, the meta-analytic
+              point estimate corrected for publication bias would be
+              {ci_text(cm$est, cm$lo, cm$hi)}.")))
+  })
+  
+  # ----------------------------------------------------------------------------
+  # svalue
+  # ----------------------------------------------------------------------------
+  
+  sval <- reactive({
+    req(input$q, input$y_col, input$v_col, cluster_col, affirmative,
+        input$model_type)
+    affirmative <- str_detect(input$direction, "\\saffirmative")
+    svalue(yi = meta_data()[[input$y_col]],
+           vi = meta_data()[[input$v_col]],
+           q = input$q, #log(input$q),
+           clustervar = meta_data()[[cluster_col()]],
+           favor.positive = affirmative(),
+           model = input$model_type)
+  })
+  
+  output$q_slider <- renderUI({
+    req(uncorrected_model)
+    m0 <- uncorrected_model()$estimate
+    q_range <- if (m0 < 0) c(m0, 0) else c(0, m0)
+    q_range <- round(q_range, 2)
+    sliderInput("q", "q (log RR)", value = 0, min = q_range[1], max = q_range[2],
+                step = 0.01)
+  })
+  
+  output$sval_est <- renderUI({
+    req(sval)
+    p(strong(glue(
+      "Publication bias required to shift point estimate to {input$q}:"
+    )),
+      br(), sval_print(sval()$sval.est))
+  })
+  
+  output$sval_ci <- renderUI({
+    req(sval)
+    p(strong(glue(
+      "Publication bias required to shift CI limit to {input$q}:"
+    )),
+    br(), sval_print(sval()$sval.ci))
+  })
+  
+  output$sval_summary <- renderUI({
+    req(input$q, input$y_col, input$v_col, cluster_col, affirmative,
+        input$model_type)
+    more_likely <- if (affirmative()) "affirmative" else "non-affirmative"
+    less_likely <- if (affirmative()) "non-affirmative" else "affirmative"
+    p(em(glue("For the meta-analytic point estimate corrected for publication
+              bias to shift to {input$q}, {more_likely} studies would
+              need to be {sval_print(sval()$sval.est)} times more likely to get
+              published than {less_likely} studies. For its CI bound to shift to
+              {input$q}, {more_likely} studies would need to be
+              {sval_print(sval()$sval.ci)} times more likely to get published
+              than {less_likely} studies.")))
+  })
+  
+  # ----------------------------------------------------------------------------
+  # significance_funnel
+  # ----------------------------------------------------------------------------
+  
+  output$funnel_plot <- renderPlot({
+    req(input$y_col, input$v_col, affirmative)
+    significance_funnel(yi = meta_data()$yi, vi = meta_data()$vi,
+                        favor.positive = affirmative()) +
+      theme_classic(base_size = 18, base_family = "Lato") +
+      theme(legend.position = "top",
+            legend.title = element_blank())
+  })
+  
+  # ----------------------------------------------------------------------------
+  # pval_plot
+  # ----------------------------------------------------------------------------
+  
+  # output$pval_plot <- renderPlot({
+  #   pval_plot(yi = meta_data()$yi, vi = meta_data()$vi) +
+  #     theme_classic(base_size = 18, base_family = "Lato") +
+  #     theme(legend.position = "top",
+  #           legend.title = element_blank())
+  # })
+  
 })
--- app/ui.R
+++ app/ui.R
@@ -1,33 +1,101 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(glue)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
+fluidPage(
+  title = "Publication bias",
+  theme = bslib::bs_theme(bootswatch = "flatly"),
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "bs-callout.css")
+  ),
+  
+  titlePanel("Sensitivity to publication bias in meta-analysis"),
+  
+  fluidRow(
+    column(
+      width = 12,
+      inputPanel(
+        fileInput("meta_data", "Upload meta-analysis data (csv)",
+                  accept = ".csv", placeholder = ""),
+        uiOutput("y_cols"),
+        uiOutput("v_cols"),
+        uiOutput("direction"),
+        uiOutput("model_type"),
+        uiOutput("cluster_cols")
+      )
     )
-))
+  ),
+  
+  # conditionalPanel(
+  # condition = "1 == 1",
+  fluidRow(
+    column(
+      width = 4,
+      div(
+        class = glue("bs-callout"),
+        h4("Estimate publication bias correction"),
+        p("For a chosen ratio of publication probabilities (η) estimate a
+          publication bias-corrected pooled point estimate and 95% confidence
+          interval."),
+        uiOutput("eta_slider"),
+        uiOutput("uncorrected"),
+        uiOutput("corrected"),
+        uiOutput("corrected_summary")
+      )
+    ),
+    # column(width = 1),
+    column(
+      width = 4, #offset = 1,
+      div(
+        class = glue("bs-callout"),
+        h4('Severity of publication bias needed to "explain away" results'),
+        p("Estimate the S-value, defined as the severity of publication bias
+          (i.e., the ratio by which affirmative studies are more likely to be
+          published than non-affirmative studies, or vice versa) that would be
+          required to shift the pooled point estimate or its confidence interval
+          limit to q."),
+        uiOutput("q_slider"),
+        # sliderInput("q", "q (risk ratio)", value = 1, min = 0.5, max = 2,
+        #             step = 0.1),
+        uiOutput("sval_est"),
+        uiOutput("sval_ci"),
+        uiOutput("sval_summary")
+      )
+    ),
+    # ),
+    
+    # fluidRow(div(style = "margin-bottom: 2em;")),
+    
+    # fluidRow(
+    column(
+      width = 4,
+      div(
+        class = glue("bs-callout"),
+        h4("Significance funnel plot"),
+        p("The estimate among only non-affirmative studies (gray diamond)
+          represents a corrected estimate under worst-case publication bias. If
+          the gray diamond represents a negligible effect size or if it is much
+          smaller than the pooled estimate among all studies (black diamond),
+          this suggests that the meta-analysis may not be robust to extreme
+          publication bias."),
+        plotOutput("funnel_plot", height = "400px", width = "450px")
+      )
+    )
+    ,
+    # column(width = 1),
+    #   column(
+    #     width = 5, offset = 1,
+    #     h4("p-value plot"),
+    #     p('Plot the one-tailed p-values. The leftmost red line indicates the
+    #     cutoff for one-tailed p-values less than 0.025 (corresponding to
+    #     "affirmative" studies; i.e., those with a positive point estimate and a
+    #     two-tailed p-value less than 0.05). The rightmost red line indicates
+    #     one-tailed p-values greater than 0.975 (i.e., studies with a negative
+    #     point estimate and a two-tailed p-value less than 0.05). If there is a
+    #     substantial point mass of p-values to the right of the rightmost red
+    #     line, this suggests that selection may be two-tailed rather than
+    #     one-tailed.'),
+    #     plotOutput("pval_plot", height = "400px", width = "450px")
+    #   )
+    # )
+  )
+)
